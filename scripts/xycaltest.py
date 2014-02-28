24ef03928753384dd2af0d37b238e85045b213f3
Scott Johnston <wamptier@gmail.com>
2014-02-28 15:03:28 -0800

    xycaltest script now in working order

--- scripts/xycaltest.py
+++ scripts/xycaltest.py
@@ -1,27 +1,7 @@
 from threading import Thread
 from approachimagingparamspark import *
 
-xycalstart = []
-ls = linescanthread(afm, [0])
-ls.start()
-while ls.is_alive():
-    xy = [getattr(daq, 'get_ai%i' % XYCHANS[0]), 
-          getattr(daq, 'get_ai%i' % XYCHANS[1])]
-    xycalstart.append(xy)
-    
-xycalend = []
-ls = linescanthread(afm, [ypoints])
-ls.start()
-while ls.is_alive():
-    xy = [getattr(daq, 'get_ai%i' % XYCHANS[0]), 
-          getattr(daq, 'get_ai%i' % XYCHANS[1])]
-    xycalend.append(xy)
-
-#Calibrate XY reading:
-xmin = min(xycalend)
-xmax = max(xycalstart)
-ymin = min(xycalstart)
-ymax = max(xycalend)
+ypoints = 128
 
 class linescanthread(Thread):
     '''
@@ -33,10 +13,41 @@ class linescanthread(Thread):
     lines: list of lines to scan
     '''
     def __init__(self, afm, lines):
-        Thread.__init__self()
+        Thread.__init__(self)
         self.afm = afm
         self.lines = lines
     def run(self):
         for line in self.lines:
             self.currentline = line
-            self.afm.LineScan(line)
+            self.afm.LineScan(line, True)
+
+afm.set_fRate(.5)
+
+xycalstart = []
+ls = linescanthread(afm, [0])
+ls.start()
+while ls.is_alive():
+    xy = [getattr(daq, 'get_ai%i' % XYCHANS[0])(), 
+          getattr(daq, 'get_ai%i' % XYCHANS[1])()]
+    xycalstart.append(xy)
+    
+xycalend = []
+ls = linescanthread(afm, [ypoints])
+ls.start()
+while ls.is_alive():
+    xy = [getattr(daq, 'get_ai%i' % XYCHANS[0])(), 
+          getattr(daq, 'get_ai%i' % XYCHANS[1])()]
+    xycalend.append(xy)
+
+#Calibrate XY reading:
+xmin = min([i[0] for i in xycalend])
+xmax = max([i[0] for i in xycalstart])
+ymin = min([i[1] for i in xycalstart])
+ymax = max([i[1] for i in xycalend])
+
+p1.clear()
+p1 = plot()
+print xmin
+print xmax
+print ymin
+print ymax
